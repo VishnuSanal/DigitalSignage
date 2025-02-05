f7770898bedc9cfe3aef12a67bd2ed15a610c23c
Vishnu Sanal T <t.v.s10123@gmail.com>
2025-02-05 12:01:31 +0530

    nooo (link)

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -9,7 +9,8 @@ object Constants {
 
     val LOGLEVEL: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC
 
-    const val DB_BASE_URL = "https://gedigitalsignage-gecpkd-default-rtdb.firebaseio.com//"
+    const val DB_BASE_URL = "https://digital-signage-gec-pkd-default-rtdb.firebaseio.com/"
+//    const val DB_BASE_URL = "https://gedigitalsignage-gecpkd-default-rtdb.firebaseio.com/"
 
     const val SCROLL_DELAY: Long = 7_500
 //    const val SCROLL_DELAY: Long = 2_500
